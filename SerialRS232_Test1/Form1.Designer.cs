46362f5cb23b389e567ff9796e1d6193f399b7dd
coman <coman@I_BETANZOS_Q>
2024-01-18 10:06:40 -0600

    Add project files.

---
+++ SerialRS232_Test1/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace SerialRS232_Test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbPorts = new System.Windows.Forms.ComboBox();
+            this.cbBaudios = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDatos = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtDatosRecibidos = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.madeBy = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.headerSoftware = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbPorts
+            // 
+            this.cbPorts.FormattingEnabled = true;
+            this.cbPorts.Location = new System.Drawing.Point(53, 35);
+            this.cbPorts.Name = "cbPorts";
+            this.cbPorts.Size = new System.Drawing.Size(173, 24);
+            this.cbPorts.TabIndex = 1;
+            // 
+            // cbBaudios
+            // 
+            this.cbBaudios.FormattingEnabled = true;
+            this.cbBaudios.Location = new System.Drawing.Point(53, 103);
+            this.cbBaudios.Name = "cbBaudios";
+            this.cbBaudios.Size = new System.Drawing.Size(173, 24);
+            this.cbBaudios.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(50, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baudios:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(51, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleccionar puerto:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 25);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Desconectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(49, 35);
+            this.textBoxStatus.Multiline = true;
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBoxStatus.Size = new System.Drawing.Size(243, 92);
+            this.textBoxStatus.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(46, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Status:";
+            // 
+            // txtDatos
+            // 
+            this.txtDatos.Location = new System.Drawing.Point(54, 279);
+            this.txtDatos.Name = "txtDatos";
+            this.txtDatos.Size = new System.Drawing.Size(121, 22);
+            this.txtDatos.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(50, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "String a enviar:";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(186, 274);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(99, 32);
+            this.btnEnviar.TabIndex = 10;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtDatosRecibidos
+            // 
+            this.txtDatosRecibidos.Location = new System.Drawing.Point(49, 234);
+            this.txtDatosRecibidos.Multiline = true;
+            this.txtDatosRecibidos.Name = "txtDatosRecibidos";
+            this.txtDatosRecibidos.Size = new System.Drawing.Size(243, 72);
+            this.txtDatosRecibidos.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(46, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Datos recibidos:";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.btnEnviar);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtDatos);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.cbBaudios);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.cbPorts);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(48, 143);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(366, 356);
+            this.panel2.TabIndex = 16;
+            // 
+            // madeBy
+            // 
+            this.madeBy.AutoSize = true;
+            this.madeBy.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.madeBy.Location = new System.Drawing.Point(456, 510);
+            this.madeBy.Name = "madeBy";
+            this.madeBy.Size = new System.Drawing.Size(399, 16);
+            this.madeBy.TabIndex = 17;
+            this.madeBy.Text = "Developed by Israel Betanzos Q - January, 2024 ©JJ Maquinados ";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textBoxStatus);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtDatosRecibidos);
+            this.panel1.Location = new System.Drawing.Point(459, 143);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(382, 356);
+            this.panel1.TabIndex = 15;
+            // 
+            // headerSoftware
+            // 
+            this.headerSoftware.AutoSize = true;
+            this.headerSoftware.Font = new System.Drawing.Font("Consolas", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headerSoftware.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.headerSoftware.Location = new System.Drawing.Point(38, 17);
+            this.headerSoftware.Name = "headerSoftware";
+            this.headerSoftware.Size = new System.Drawing.Size(515, 79);
+            this.headerSoftware.TabIndex = 0;
+            this.headerSoftware.Text = "RS-232 TESTER";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SerialRS232_Test1.Properties.Resources.JJ_Maquinados_logo_INVERTIDO;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 103);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(48, 13);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(189, 111);
+            this.panel3.TabIndex = 18;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.headerSoftware);
+            this.panel4.Location = new System.Drawing.Point(252, 13);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(589, 111);
+            this.panel4.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ClientSize = new System.Drawing.Size(881, 535);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.madeBy);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RS-232 Testing Machine v1_0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbPorts;
+        private System.Windows.Forms.ComboBox cbBaudios;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDatos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtDatosRecibidos;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label madeBy;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label headerSoftware;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ SerialRS232_Test1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Windows.Forms.VisualStyles;
+using System.Threading;
+
+namespace SerialRS232_Test1
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort; // Declaracion del puerto serial como objeto
+        public Form1()
+        {
+            InitializeComponent();
+            // Inicializa la instancia del SerialPort y configura el ComboBox con los puertos disponibles.
+            CargarPuertosDisponibles();
+            CargarBaudios();
+            serialPort = new SerialPort();
+
+            // Asociar el evento DataReceived al método serialPort_DataReceived
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(serialPort_DataReceived);
+        }
+
+        private void CargarPuertosDisponibles()
+        {
+            string[] puertos = SerialPort.GetPortNames();
+            cbPorts.Items.AddRange(puertos);
+        }
+
+        private void CargarBaudios()
+        {
+            int[] baudios = { 9600, 19200, 38400, 57600, 115200 };
+
+            // Convertir los enteros a objetos antes de agregar al ComboBox
+            object[] baudiosObjects = baudios.Cast<object>().ToArray();
+            cbBaudios.Items.AddRange(baudiosObjects);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cbPorts.SelectedItem != null && cbBaudios.SelectedItem != null)
+            {
+                try
+                {
+                    serialPort.PortName = cbPorts.SelectedItem.ToString();
+                    serialPort.BaudRate = (int)cbBaudios.SelectedItem;
+                    serialPort.Open();
+
+                    // Verificar si el puerto se ha abierto correctamente
+                    if (serialPort.IsOpen)
+                    {
+                        MessageBox.Show("Conexión establecida correctamente.", "Conexión Exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        // Muestra el mensaje en un TextBox llamado txtStatus
+                        textBoxStatus.Text = "Conexión establecida correctamente.";
+                        textBoxStatus.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al conectar: No se pudo abrir el puerto.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        // Muestra un mensaje de error en el TextBox
+                        textBoxStatus.Text = "Error al conectar: No se pudo abrir el puerto.";
+                        textBoxStatus.ForeColor = Color.Red;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error al conectar: " + "'PortName' no se puede establecer mientras el puerto está abierto.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); //ex.Message
+                    // Muestra un mensaje de error en el TextBox
+                    textBoxStatus.Text = "Error al conectar: " + "'PortName' no se puede establecer mientras el puerto está abierto."; //ex.Message
+                    textBoxStatus.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un puerto y velocidad de baudios antes de conectar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                // Muestra un mensaje de advertencia en el TextBox
+                textBoxStatus.Text = "Selecciona un puerto y velocidad de baudios antes de conectar.";
+                textBoxStatus.ForeColor = Color.Orange;
+            }
+        }
+
+
+        private bool datosEnviados = false;
+        // Declaración de la variable para almacenar el código DataMatrix
+        //private string codigoDataMatrix = "+";
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen && !string.IsNullOrEmpty(txtDatos.Text))
+            {
+                try
+                {
+                    // Agrega un mensaje antes de enviar
+                    txtDatosRecibidos.Text = "Enviando datos: " + txtDatos.Text;
+
+                    // Aquí se utiliza el puerto y la velocidad de baudios configurados
+                    serialPort.WriteLine(txtDatos.Text);
+
+                    // Indica que los datos han sido enviados
+                    datosEnviados = true;
+
+                    MessageBox.Show("Datos enviados correctamente.", "Envío Exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al enviar datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Asegúrate de que el puerto esté conectado y proporciona datos para enviar.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Este evento se llama cuando se reciben datos en el puerto serial
+            try
+            {
+                string datosRecibidos = serialPort.ReadLine(); // Lee una línea de datos
+
+                // Puedes realizar algún procesamiento con los datos recibidos antes de mostrarlos
+                // Por ahora, simplemente los mostramos en el TextBox llamado txtDatosRecibidos
+                this.Invoke(new Action(() => { txtDatosRecibidos.Text = datosRecibidos; }));
+
+                MessageBox.Show("Datos recibidos correctamente: " + datosRecibidos, "Recepción Exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al recibir datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Asegúrate de cerrar el puerto serial antes de cerrar la aplicación
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+    }
+}
